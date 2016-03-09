6a6fa7942462779fdc1d4ccb5ad18679c85f2fe9
Midas Lambrichts <MidasLamb@users.noreply.github.com>
2016-03-09 11:36:21 +0100

    Update Oefenzitting5.m

--- Sem2/NumeriekeWiskunde/Oefenzittingen/Oefenzitting5.m
+++ Sem2/NumeriekeWiskunde/Oefenzittingen/Oefenzitting5.m
@@ -1,14 +1,17 @@
-% Probleem 1
+% Probleem 1 -------------------------------------------------------------------------------
 n = -1:-0.1:-15
 h = 10.^n
 
 y1 = (5*exp(h) - 5)./h
 y2 = (5*exp(h) - 5*exp(-h))./(2*h)
 
-rel1 = abs(5-y1)/5
+rel1 = abs(5-y1)/5                %Relatieve fout is (verwacht-uitgekomen)/verwacht
 rel2 = abs(5-y2)/5
 
 figure
 loglog(h, rel1)
-hold on
+hold on                           %Nodig zodat beide plots getekend worden, ofanders wordt de eerste door de tweede overschreven.
 loglog(h, rel2)
+
+
+% Probleem 2 -------------------------------------------------------------------------------
